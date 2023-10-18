c5b896cc1bd3e52992190f6ff0fcd69cd7e1409c
KPGROUP\Anusorn.ket <anusornketkaew0@gmail.com>
2023-10-18 16:52:23 +0700

    first comit

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import { BrowserRouter,  Routes, Route, Link , NavLink } from "react-router-dom";
+
+import Index from './pages';
+import Home from './pages/Home';
 
 export default function App() {
   return (
-    <div>
-      
-    </div>
+    <BrowserRouter>
+      <Routes>
+      <Route path="/eContractfarm" element={<Index />}>
+        <Route path="" element={<Home />} />
+      </Route>
+    </Routes>
+    </BrowserRouter>
   )
 }
